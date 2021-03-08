e8673a8dc1113dd0eb2e781444ee7dace7b0a373
NeilBuison <79896983+NeilBuison@users.noreply.github.com>
2021-03-08 06:00:12 -0500

    Create Main.java
    Step 9

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+  private String Fname1 = "Neil";
+  private String Lname1 = "Buison";
+  private String Fname2 = "Xhar John";
+  private String Lname2 = "Suarez";
+  private String Fname3 = "Paul John";
+  private String Lname3 = "Buenaflor";
+  private String Fname4 = "Jared";
+  private String Lname4 = "Lanza";
+  private String Fname5 = "Miguel";
+  private String Lname5 = "Javelona";
+  
+  public static void main (String[]args){
+    Main myObj = new Main();
+    System.out.println("First Person: " + myObj.Fname1 + " " + myObject.Lname1);
+    System.out.println("First Person: " + myObj.Fname2 + " " + myObject.Lname2);
+    System.out.println("First Person: " + myObj.Fname3 + " " + myObject.Lname3);
+    System.out.println("First Person: " + myObj.Fname4 + " " + myObject.Lname4);
+    System.out.println("First Person: " + myObj.Fname5 + " " + myObject.Lname5);
+  }
+}
+    
