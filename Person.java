42f05772426bcdd83ba9b890bf0431c76ae20a7d
NeilBuison <79896983+NeilBuison@users.noreply.github.com>
2021-03-08 05:05:18 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,10 @@
 public class Main {
   String fname = "Neil Patrick";
   String lname = "Buison";
+  
+  public static void main(String[]args){
+  Main myName = new Main();
+  System.out.println("First Name: " + myName.fname);
+  System.out.println("Last Name: " + myName.lname);
+  }
 }
