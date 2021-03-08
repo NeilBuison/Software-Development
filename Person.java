3926a987837cc0bb14df76767f1c2a8fcc9422f2
NeilBuison <79896983+NeilBuison@users.noreply.github.com>
2021-03-08 05:29:18 -0500

    Update Person.java
    Step 7

--- Person.java
+++ Person.java
@@ -1,10 +1,50 @@
-public class Main {
-  String fname = "Neil Patrick";
-  String lname = "Buison";
+public class Person
+{
+  private String Fname;
+  private String Lname;
   
-  public static void main(String[]args){
-  Main myName = new Main();
-  System.out.println("First Name: " + myName.fname);
-  System.out.println("Last Name: " + myName.lname);
+  public String getName(){
+    return Fname;
   }
-}
+  public String getName(){
+    return Lname:
+  }
+  
+  public class Person {
+    String fname;
+    String lname;
+  
+    
+    public static void main (String[]args){
+      Person p = new Person();
+      
+      p.setFname("Neil Patrick");
+      p.setLname("Buison");
+      System.out.println(p.getFname());
+      System.out.println(p.getLname());
+      
+      p.printDetails();
+    }
+    
+    public void setFname(String Fname){
+      this.fname=fname;
+    }
+    public void setlname(String lname){
+      this.Lname=Lname;
+    }
+    
+    public String getFname(){
+      return this.Fname;
+    }
+    public String getLname(){
+      return this.Lname;
+    }
+    
+    public void printDetails(){
+      System.out.println(getFname + " " + getLname);
+    }
+  }
+
+    
+  
+  
